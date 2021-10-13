0cbb4f854b8c09af96883effb55d6865a2beab97
Prakhar Dhama <pdhama161@gmail.com>
2021-10-13 11:27:38 +0530

    connection to mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
 
 const app = express();
 const port = 3000;
 
 //view engine
 app.set('view engine','ejs');
+
+//connect to mongodb
+mongoose.connect(keys.mongodb.dbURI,{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('connected to mongodb'))
+.catch((err) => console.log(err));
 
 //routes
 app.use('/auth',authRoutes);
