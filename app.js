a66ffc02007cac8ca5c84240032040f905a62aa7
Prakhar Dhama <pdhama161@gmail.com>
2021-10-14 08:47:08 +0530

    cookie session and initialize passport in app.js

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
+const passport = require('passport');
 
 const app = express();
 const port = 3000;
 
 //view engine
 app.set('view engine','ejs');
+
+app.use(cookieSession({
+    //age of cookie
+    maxAge: 24 * 60 * 60 * 1000,
+    //key to encrypt the cookie
+    keys: [keys.session.cookieKey]
+}));
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connect to mongodb
 mongoose.connect(keys.mongodb.dbURI,{ useNewUrlParser: true, useUnifiedTopology: true})
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -9,7 +9,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser((id, done) => {
     User.findById(id).then((user) => {
-        done(null, id);
+        done(null, user);
     })
 });
 
